82c5314f4c9d2fc5225442d491814500dd2c0394
Victor <victor_ribeiro96@outlook.com>
2023-03-14 16:06:24 -0300

    Adicionando primeiros arquivos

---
+++ src/main/java/br/es/tj/eprocees/EproceesApplication.java
@@ -0,0 +1,13 @@
+package br.es.tj.eprocees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EproceesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EproceesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/es/tj/eprocees/EproceesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.es.tj.eprocees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EproceesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
