f1b7ab5a7bb656e9d302cb3849578717f99dc2c1
mauriliox21 <mauriliox21@gmail.com>
2023-03-15 14:59:50 -0300

    Adicionar bootstrap e jquery

--- src/main/java/br/es/tj/eprocees/controller/IndexController.java
+++ src/main/java/br/es/tj/eprocees/controller/IndexController.java
@@ -1,4 +1,12 @@
 package br.es.tj.eprocees.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class IndexController {
+    @GetMapping("/cadastrar_impedimento")
+    public String cadastrarImpedimento(){
+        return "cadastrar_impedimento";
+    }
 }
