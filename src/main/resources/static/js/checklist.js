84993c64ef58f1d7b152ed7d355cd1c3cebca0d1
João Pedro <joaoooh@gmail.com>
2023-03-17 16:05:38 -0300

    Finalização redistribuição

--- src/main/resources/static/js/checklist.js
+++ src/main/resources/static/js/checklist.js
@@ -13,3 +13,23 @@ function limpaSelecao(){
         ele[i].checked=false;  
     }  
 }
+
+
+function selecionaLinhaTabela(param){
+    var cols = $("#tabela tbody tr:nth-child("+param+") td");
+    console.log(cols);
+    var obj = {};
+    for(var i=0; i < cols.length; i++){
+        obj["col"+i] = cols[i].innerText;
+    }
+    // $.ajax({
+    //     type: "POST",                     
+    //     url:"http://localhost:8080/redistribuir_processo",                                                 
+    //     data: JSON.stringify(obj),
+    //     cache: false,
+    //     dataType: "json",
+    //     contentType: "application/json; charset=utf-8",
+    //     success: function (json) {
+    //     }});
+}
+
