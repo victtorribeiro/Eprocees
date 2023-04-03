ff1a3eefa6f117a25d78d752b0427600555f09d8
Joao Pedro Mendonca Oliveira <joao.mendoncao@sonda.com>
2023-04-03 10:17:15 -0300

    Pesquisa relator

--- src/main/java/br/es/tj/eprocees/repository/pessoa/PessoaFisicaRepository.java
+++ src/main/java/br/es/tj/eprocees/repository/pessoa/PessoaFisicaRepository.java
@@ -7,11 +7,19 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import br.es.tj.eprocees.model.PesquisarMagistrado;
 import br.es.tj.eprocees.model.entity.pessoa.PessoaFisica;
+import br.es.tj.eprocees.model.entity.retornoQueries.RetornoPesquisaMagistrado;
+import br.es.tj.eprocees.model.entity.retornoQueries.RetornoPesquisaRelator;
 
 @Repository
-public interface PessoaFisicaRepository extends JpaRepository<PessoaFisica, Long>{
+public interface PessoaFisicaRepository extends JpaRepository<PessoaFisica, Integer> {
     @Query(name = "PesquisarMagistrado", nativeQuery = true)
-    public List<PesquisarMagistrado> pesquisarMagistrado(@Param("nmPessoa") String nmPessoa, @Param("cdMatriculaRh") String cdMatriculaRh);
+    public List<RetornoPesquisaMagistrado> pesquisarMagistrado(@Param("nmPessoa") String nmPessoa,
+            @Param("cdMatriculaRh") String cdMatriculaRh);
+
+    @Query(name = "PesquisarRelator", nativeQuery = true)
+    // falta passar o parametro ORGAO_ID
+    public List<RetornoPesquisaRelator> pesquisarRelator(@Param("nmPessoa") String nmPessoa,
+            @Param("cdMatricula") String cdMatricula,
+            @Param("P_ID_ORGAO") Integer P_ID_ORGAO);
 }
